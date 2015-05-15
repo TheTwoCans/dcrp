a84828c7d03bc584e37480175526d318362dd8fe
Ali Lichtenberg <TheTwoCans@gmail.com>
2015-05-15 14:12:44 -0700

    Added civee lawbook

---
+++ plugins/books/items/books/sh_book_lawbook.lua
@@ -0,0 +1,14 @@
+ITEM.name = "Laws and Responsibilities"
+ITEM.model = "models/props_lab/binderblue.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.desc = "A thick book that every citizen owns."
+
+ITEM.isURL = false
+ITEM.content = [[
+<br>1- Citizens must obey orders from all government workers.
+<br>2- Attempts to harm service members or peace officers will be met with inprisonment or death.
+<br>3- Being outside during citywide curfew will result in your arrest.
+<br>4- 
+
+]]
