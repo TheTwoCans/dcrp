11e42657db1643471dc1959ed532df8a29fdba87
ragingpandas <bassil.shama@gmail.com>
2015-05-15 21:27:51 -0700

    More military guidelines

---
+++ plugins/books/items/books/sh_book_miltaryhandbook.lua
@@ -0,0 +1,17 @@
+ITEM.name = "Military Guidelines"
+ITEM.model = "models/props_lab/binderblue.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.desc = "A handbook given out to all service members."
+
+ITEM.isURL = false
+ITEM.content = [[
+<br>1- Follow all orders given out by comanding officers, anyone who disobayes this will be shot.</br>
+<br>2- Do not fire on citzens, unless attacked or you are under orders to.</br>
+<br>3- If issued a weapon, this weapon is yours, and only yours. If you sell or give away any weapons issued to you, you will be charged for treason.</br>
+<br>4- Never share confidencial information.</br>
+<br>5- Do not ask to be promoted, your commanding officer will deem if you are fit for a promotion.</br>
+<br>6- Never fire at anoter service member, unless under direct orders from your commanding officer.</br>
+<br>7- Deserting a mission, is considered treason, and you will be shot.</br>
+
+]]
