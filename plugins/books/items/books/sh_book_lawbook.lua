fce876a2e176ba33086c1310c8fb09bdeb366207
Ali Lichtenberg <TheTwoCans@gmail.com>
2015-05-18 14:09:33 -0700

    Citizen lawbook continued

--- plugins/books/items/books/sh_book_lawbook.lua
+++ plugins/books/items/books/sh_book_lawbook.lua
@@ -1,4 +1,4 @@
-ITEM.name = "Laws and Responsibilities"
+ITEM.name = "Laws, Responsibilities, and Regulations"
 ITEM.model = "models/props_lab/binderblue.mdl"
 ITEM.width = 1
 ITEM.height = 1
@@ -6,9 +6,32 @@ ITEM.desc = "A thick book that every citizen owns."
 
 ITEM.isURL = false
 ITEM.content = [[
+<h1>LAWS</h1>
 <br>1- Citizens must obey orders from all government workers.
 <br>2- Attempts to harm service members or peace officers will be met with inprisonment or death.
 <br>3- Being outside during citywide curfew will result in your arrest.
-<br>4- 
+<br>4- Stealing is punishable depending on severity of the crime.
+<br>5- Plotting against the government is illegal and will be punished.https://www.youtube.com/
+<br>6- Posession of CLASS 2 substances or higher without proper clearence is illegal.
+<br>7- Posession of a CLASS 2 weapon or higher without proper clearence is illegal.
+<br>
+<h1>RESPONSIBLITIES</h1>
+<br>Citizens are responsible for helping the Nation by doing their jobs.
+These jobs could be anything. Military service, Police service, or a generic job will help
+ensure the wellbeing of our great Nation. Compliance with all government personell is required. The 
+92nd Regiment National Sentinel and Federal Emergency Police are here to help, so please comply.
+<br>
+<h1>SUBSTANCE REGULATIONS</h1>
+<br>CLASS 1: Basic Pharmaceuticals and Medical Supplies
+<br>CLASS 2: Mid-Tier Pharmaceuticals and Medical Supplies, Illegal Drugs
+<br>CLASS 3: High Tier Pharmaceuticals and Medical Supplies, High Tier Illegal Drugs, Toxens and Poisons
+<br>CLASS 4: Bioweapons, leathal and nonleathal.
+<br>
+<h1>FIREARM REGULATIONS</h1>
+<br>CLASS 1: Blades, Batons, or any other melee weapon incapable of firing a projectile.
+<br>CLASS 2: Single/Double Barrel Shotguns and Bolt-Action rifles chambered in non Mil-Spec calibers.
+<br>CLASS 3: Semiautomatic weapons chambered in any caliber.
+<br>CLASS 4: Military-Grade Fully-Automatic weapons or explosive devices.
+
 
 ]]
