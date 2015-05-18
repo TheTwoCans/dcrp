64a986b4a956d4dbbbc0a4836407619af5da4f71
ragingpandas <bassil.shama@gmail.com>
2015-05-18 13:47:34 -0700

    stuff
    More

---
+++ plugins/books/items/books/sh_book_policeguide.lua
@@ -0,0 +1,15 @@
+ITEM.name = "Police Handbook"
+ITEM.model = "models/props_lab/binderblue.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.desc = "A handbook given out to all members of the police force."
+
+ITEM.isURL = false
+ITEM.content = [[
+Welcome to the police force recruit. 
+This handbook is a standard issue handbook that covers rules and guidelines that all police officers must follow.
+Firstly, all members of the police force must follow the rules set out by the standard military handbook.
+Second, you will only carry a firearm if, and only if you are given firearm clearence.
+Third, you
+
+]]
