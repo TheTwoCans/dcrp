6e9dd5a0269a9bf2e9399afadf0329ea622d8de2
ragingpandas <bassil.shama@gmail.com>
2015-05-19 13:39:37 -0700

    Added police book
    adad

--- plugins/books/items/books/sh_book_policeguide.lua
+++ plugins/books/items/books/sh_book_policeguide.lua
@@ -3,13 +3,43 @@ ITEM.model = "models/props_lab/binderblue.mdl"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.desc = "A handbook given out to all members of the police force."
+  font-family: 'Nanum Brush Script', serif;
+        font-size: 19px;
+        color: black;
 
+		background: url('http://i.imgur.com/BhjRYop.png');
+		width: 95%;
+		height: 350px; 
 ITEM.isURL = false
 ITEM.content = [[
-Welcome to the police force recruit. 
-This handbook is a standard issue handbook that covers rules and guidelines that all police officers must follow.
-Firstly, all members of the police force must follow the rules set out by the standard military handbook.
-Second, you will only carry a firearm if, and only if you are given firearm clearence.
-Third, you
-
+<h1>Welcome to the police force recruit.</h1>
+<br>This handbook is a standard issue handbook that covers rules and guidelines that all police officers must follow.
+<br>Firstly, all members of the police force must follow the rules set out by the standard military handbook.
+<br>Second, you will only carry a firearm if, and only if you are given firearm clearence.
+<br>Third, you will never attack a citzen unless you are attacked.
+<h1>Pentaly Guide</h1>
+<h3><strong>Level One Offense</strong></h3>
+<br>Criminal Misdomeaner
+<br>Failurtating in banned areas.
+<br>Failure to identify self
+<br>Excessive swearing, or insults.
+<br>Solicicious activities.
+<br>Harrasment.
+<h4>Penalties include warning, floging, or in extreme instances 30 minute prison time.</h4>
+<h3><strong>Level Two Offense</strong></h3>
+<br>Petty theiving.
+<br>Possesion of a level two pharmaceutical.
+<br>Possesion of a level two firearm or weapon.
+<br>Intimidation.
+<br>Annoying a government worker.
+<h4>Penalties include public floging, interigation, 30 minutes - 2 hours of jail, or up to $100</h4>
+<h3><strong>Level Three Offense</strong></h3>
+<br>Stealing.
+<br>Minor Fraud.
+<br>Ignoring a government offical.
+<br>Illegal dealing of a level two pharmaceutical.
+<br>Illegal dealing of a level two weapon.
+<br>Possesion of a level three pharmaceutical.
+<br>Possesion of a level three firearm or weapon.
+<h4>Penalties include public floging, interigation, 30 minutes - 2 hours of jail, or up to $100</h4>
 ]]
