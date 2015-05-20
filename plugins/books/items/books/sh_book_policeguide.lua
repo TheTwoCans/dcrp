95141933bf35ecb406ffa8e9181f3975d501329d
ragingpandas <bassil.shama@gmail.com>
2015-05-20 13:08:54 -0700

    Finito Police
    FINAALLLLLLLLLLLLLLLLLLLLY

--- plugins/books/items/books/sh_book_policeguide.lua
+++ plugins/books/items/books/sh_book_policeguide.lua
@@ -3,13 +3,31 @@ ITEM.model = "models/props_lab/binderblue.mdl"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.desc = "A handbook given out to all members of the police force."
-  font-family: 'Nanum Brush Script', serif;
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/earlyaccess/nanumbrushscript.css">
+    <style>
+      body {
+        font-family: 'Nanum Brush Script', serif;
         font-size: 19px;
         color: black;
 
 		background: url('http://i.imgur.com/BhjRYop.png');
 		width: 95%;
 		height: 350px; 
+      }
+      .title {
+        font-family: 'Nanum Brush Script', serif;
+        font-size: 40px;
+        color: black;
+      }
+      .author {
+        font-family: 'Nanum Brush Script', serif;
+        font-size: 16px;
+        color: black;
+      }
+    </style>
+  </head>
 ITEM.isURL = false
 ITEM.content = [[
 <h1>Welcome to the police force recruit.</h1>
@@ -19,27 +37,39 @@ ITEM.content = [[
 <br>Third, you will never attack a citzen unless you are attacked.
 <h1>Pentaly Guide</h1>
 <h3><strong>Level One Offense</strong></h3>
-<br>Criminal Misdomeaner
-<br>Failurtating in banned areas.
-<br>Failure to identify self
+<br>Criminal Misdomeaner.
+<br>Solicitating in banned areas.
+<br>Failure to identify self.
 <br>Excessive swearing, or insults.
-<br>Solicicious activities.
+<br>Mliciciouslicicious activities.
 <br>Harrasment.
-<h4>Penalties include warning, floging, or in extreme instances 30 minute prison time.</h4>
+<h4>Penalties include warning, flogging, or in extreme instances 30 minutes prison time.</h4>
 <h3><strong>Level Two Offense</strong></h3>
 <br>Petty theiving.
-<br>Possesion of a level two pharmaceutical.
-<br>Possesion of a level two firearm or weapon.
+<br>Possesion of a level two pharmaceutical without permit.
 <br>Intimidation.
 <br>Annoying a government worker.
-<h4>Penalties include public floging, interigation, 30 minutes - 2 hours of jail, or up to $100</h4>
+<h4>Penalties include public flogging, interrogation, 30 minutes - 2 hours of jail, or a fine up to $100</h4>
 <h3><strong>Level Three Offense</strong></h3>
 <br>Stealing.
 <br>Minor Fraud.
-<br>Ignoring a government offical.
-<br>Illegal dealing of a level two pharmaceutical.
-<br>Illegal dealing of a level two weapon.
-<br>Possesion of a level three pharmaceutical.
-<br>Possesion of a level three firearm or weapon.
-<h4>Penalties include public floging, interigation, 30 minutes - 2 hours of jail, or up to $100</h4>
+<br>Possesion of a level three firearm or weapon without permit.
+<h4>Penalties include, interigation, 2 hours - 12 hours of jail, or up to $1000 fine.</h4>
+<h3><strong>Level Four Offense</strong></h3>
+<br>Possesion of a level four pharmaceutical without permit.
+<br>Possesion of a level four firearm or weapon without permit.
+<br>Illegal dealing of a level three pharmaceutical.
+<br>Illegal dealing of a level three weapon.
+<br>Minor terrorism.
+<br>Fraud.
+<br>Mansluater.
+<h4>Penalties include interrogation, 12 - 72 hours of jail, a fine up to $2500, or  hard labor.</h4>
+<h3><strong>Level Five Offense</strong></h3>
+<br>Illegal dealing of a four three pharmaceutical.
+<br>Illegal dealing of a four three weapon.
+<br>Murder.
+<br>Resisting arrest.
+<br>Terrorism.
+<h4>Penalties include death, 72 hours - 1 week of jail, or a fine up to $25000.</h4>
 ]]
+</html>
