c59211c8ce1e621b868ad8c3241eaf0462c05615
ragingpandas <bassil.shama@gmail.com>
2015-05-26 13:40:25 -0700

    Recuritent Form
    Copied a google doc

---
+++ plugins/books/items/books/sh_book_recruitmentform.lua
@@ -0,0 +1,39 @@
+ITEM.name = "Join the National Sentinel!"
+ITEM.model = "models/props_lab/binderblue.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.desc = "A form to generate interest in a wonderful government employment option."
+
+ITEM.isURL = false
+ITEM.content = [[
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/earlyaccess/nanumbrushscript.css">
+    <style>
+      body {
+        font-family: 'Nanum Brush Script', serif;
+        font-size: 19px;
+        color: black;
+
+		background: url('http://i.imgur.com/BhjRYop.png');
+		width: 95%;
+		height: 350px; 
+      }
+      .title {
+        font-family: 'Nanum Brush Script', serif;
+        font-size: 40px;
+        color: black;
+      }
+      .author {
+        font-family: 'Nanum Brush Script', serif;
+        font-size: 16px;
+        color: black;
+      }
+    </style>
+  </head>
+  <body>
+  	Hey there, citizen! Do you want a grand place in society? Free college? Huge benefits? Then join the Nation’s 92nd Regiment National Sentinel! To enlist, head to a recruitment station when said so over the city broadcast. When it is your turn in line, answer the questions the recruiter asks. After you’ve been accepted go home and get that well-needed rest. Then the city broadcast will ask all accepted recruits to report to the training grounds. Once you arrive your adventure starts!
+
+  </body>
+</html>
+]]
