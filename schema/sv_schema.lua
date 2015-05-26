3bbd5c65042ae54afeb8c5df0548d1753fb58919
ragingpandas <bassil.shama@gmail.com>
2015-05-26 14:10:49 -0700

    fixed form

---
+++ schema/sv_schema.lua
@@ -0,0 +1,38 @@
+nut.util.include("cl_hooks.lua")
+nut.util.include("sh_hooks.lua")
+SCHEMA.helps = {
+["Rules List"] = [[
+	<br>1- Type using correct punctuation, syntax, and spelling.
+	<br>2- All actions must be accommodated with a “/me” explaining the action.
+	<br>3- Act realistically as if your character would.
+	<br>4- Do not Metagame (Using OOC information for IC purposes)
+	<br>5- Do not Powergame (Using “/me”s to force actions on other players)
+	<br>6- Keep foul language in chat only used when necessary.
+	<br>7- All characters must be IC 15 years old or older.
+	<br>8- ERP or any form of RP that includes procreation, whether voluntary or involuntary  is strictly prohibited and it’s just sad.
+	<br>9- PainRP (Responding realistically to pain IC)  must be followed whenever a character is injured.
+	<br>10- FailRP (Breaking fundamental RP rules while IC) is strictly prohibted.]],
+
+	["Server Lingo"] = [[
+	<br>RP -  RolePlay
+	<br>IC - In Character
+	<br>OOC - Out Of Character
+	<br>LOOC - Local Out Of Character
+	<br>Metagame/Metagaming - Using OOC information for IC purposes
+	<br>Powergame/Powergaming - Using “/me”s to force actions on other players
+	<br>PainRP - Responding realistically to pain IC]],
+
+	["Basic Commands"] = [[
+	<br>TAB - Open Player List
+	<br>F1 -  Open Game Menu
+	<br>F3 -  Open Recognition Menu
+	<br>/me - Talk in third person. EXAMPLE: “/me eats food.” goes to “[Character Name] eats food.”
+	<br>/it - Describe something in the third person. EXAMPLE: “/it The old wound appears to be healed.” goes to “The old wound appears to be healed.”
+	<br>/fallover -  Makes your character fall over. USES: Sleeping, Fainting, Falling Over, etc.
+	<br>/whisper - Talk like normal, but the hearing radius will be smaller.
+	<br>/yell - Talk like normal, but the hearing radius will be larger.
+	<br>/radio - Say something over a radio frequency, assuming you have a handheld radio.]],
+
+
+
+}
