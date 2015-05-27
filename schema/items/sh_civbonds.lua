8c1c26823718628a153574f0a3ad8fd9ac6e9d3b
Ali Lichtenberg <TheTwoCans@gmail.com>
2015-05-27 13:18:17 -0700

    set civee bond dimensions

--- schema/items/sh_civbonds.lua
+++ schema/items/sh_civbonds.lua
@@ -2,3 +2,5 @@ ITEM.name = "Civilian Bonds"
 ITEM.desc = "A small piece of paper resembling a bank note. This can be sold at any retailer. These are given out by the government as pay or as reward to regular citizens and military. If the price falls to sharply the government will by back."
 ITEM.model = "models/props_lab/clipboard.mdl"
 ITEM.price = 100
+ITEM.height = 1
+ITEM.width = 1
