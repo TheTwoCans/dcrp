9b6925609b2909ecee85bde302783b7e4229668c
ragingpandas <bassil.shama@gmail.com>
2015-05-27 13:22:59 -0700

    Added dimensions to gov bonds
    40th commit!

--- schema/items/sh_govbonds.lua
+++ schema/items/sh_govbonds.lua
@@ -2,3 +2,5 @@ ITEM.name = "Government Bonds"
 ITEM.desc = "A small piece of paper resembling a bank note. These bonds are sold by the government when they are short on cash, they will not always be sold. They can be sold at the bond office, when the government is ready to pay back debts, the selling price is often higher than the purchase. It is illegal to buy and sell these from a non authorized retailer."
 ITEM.model = "models/Items/battery.mdl"
 ITEM.price = 100
+ITEM.height = 1
+ITEM.width = 1
